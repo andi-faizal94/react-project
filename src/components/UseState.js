b79c7928377eda3e421fd00d9d6446e0cb84d146
Andi Faizal <andifaizal647@gmail.com>
2022-01-26 22:05:41 +0700

    add multiply and divide in UseState.js

--- src/components/UseState.js
+++ src/components/UseState.js
@@ -2,20 +2,31 @@ import React, { useState } from "react";
 
 function UseStates() {
   let [counter, setCounter] = useState(0);
-  console.log(setCounter, counter);
+  //   console.log(setCounter, counter);
+  console.log(counter);
   const increment = () => {
-    setCounter(counter + 1);
+    setCounter(counter + 2);
   };
 
   const decrement = () => {
-    setCounter(counter - 1);
+    setCounter(counter - 2);
+  };
+
+  const multiply = () => {
+    setCounter(counter * 2);
+  };
+
+  const divide = () => {
+    setCounter(counter / 2);
   };
   return (
     <div>
       <span>{counter}</span>
       <br />
-      <button onClick={increment}>Add</button>
-      <button onClick={decrement}>Min</button>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+      <button onClick={divide}>:</button>
+      <button onClick={multiply}>x</button>
     </div>
   );
 }
